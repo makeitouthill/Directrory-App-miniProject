065b982f0a248bf2a81a94de4c8818ec1ff161cb
DBENSAN <dftb97@gmail.com>
2023-02-28 12:02:37 -0800

    adds webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,9 +1,17 @@
-// TODO: Install the following package:
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+   openDB('', 1, {
+      upgrade(db) {
+         if (db.objectStoreNames.contains('')) {
+           console.log(' database already exists');
+           return;
+         }
+         db.createObjectStore('', { keyPath: 'id', autoIncrement: true });
+         console.log(' database created');
+       },
+   })
 };
 
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -11,17 +14,54 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: ''
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Web App!',
+        background_color: '#ffffff',
+        // crossorigin: 'anonymous', 
+        //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] 
+            // multiple sizes
+          }
+        ]
+      }),
     ],
+    
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
     }
   };
 };
